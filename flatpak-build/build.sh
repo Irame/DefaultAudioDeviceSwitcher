2c6518e5cffab26dad36bec682f91fd56268aca7
Felix <irame94@gmail.com>
2025-12-08 23:20:05 +0100

    moved flatpak resources

---
+++ flatpak-build/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+python3 flatpak-dotnet-generator.py --dotnet 10 --freedesktop 24.08 \
+    nuget-sources.json \
+    ../DefaultAudioDeviceSwitcher.Linux/DefaultAudioDeviceSwitcher.Linux.csproj
+
+flatpak-builder build-dir --user --force-clean --install --repo=repo --default-branch=1.0.0 com.github.irame.DefaultAudioDeviceSwitcher.yaml
+
