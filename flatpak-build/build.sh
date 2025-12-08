005ba1a56dbb6b165b6456792d1f3c1f8fcaf1c3
Felix <irame94@gmail.com>
2025-12-09 00:48:28 +0100

    switched to constant branches for flatpak builds

--- flatpak-build/build.sh
+++ flatpak-build/build.sh
@@ -5,5 +5,5 @@ python3 flatpak-dotnet-generator.py --dotnet 10 --freedesktop 24.08 \
     nuget-sources.json \
     ../DefaultAudioDeviceSwitcher.Linux/DefaultAudioDeviceSwitcher.Linux.csproj
 
-flatpak-builder build-dir --user --force-clean --install --repo=repo --default-branch=1.0.0 com.github.irame.DefaultAudioDeviceSwitcher.yaml
+flatpak-builder build-dir --user --force-clean --install --repo=repo --default-branch=dev com.github.irame.DefaultAudioDeviceSwitcher.yaml
 
